bc5888423e36616d359b68ff86ae8a8fca297a47
Maksim Kot <kotmaksimarturovich@gmail.com>
2025-04-29 19:27:23 +0300

    feat(web): add main.go

---
+++ web/cmd/web/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+
+	"github.com/Maksim-Kot/Tech-store-web/config"
+	"github.com/Maksim-Kot/Tech-store-web/internal/controller/web"
+	cataloggateway "github.com/Maksim-Kot/Tech-store-web/internal/gateway/catalog/http"
+	httphandler "github.com/Maksim-Kot/Tech-store-web/internal/handler/http"
+	httpserver "github.com/Maksim-Kot/Tech-store-web/internal/server/http"
+)
+
+func main() {
+	cfg, err := config.New("base.yaml")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cataloggateway := cataloggateway.New("localhost:4001")
+
+	ctrl := web.New(cataloggateway)
+	h := httphandler.New(ctrl)
+
+	srv := httpserver.New(h, cfg.Api)
+	err = srv.Serve()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
