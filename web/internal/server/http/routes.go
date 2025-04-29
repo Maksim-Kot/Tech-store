b54d4c8e2fa50bb9c2aca116f1a3d8500f4a106d
Maksim Kot <kotmaksimarturovich@gmail.com>
2025-04-29 19:26:49 +0300

    feat(web): add server structure to handle HTTP API setup

---
+++ web/internal/server/http/routes.go
@@ -0,0 +1,17 @@
+package http
+
+import "net/http"
+
+func (s *Server) routes() *http.ServeMux {
+	router := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	router.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+
+	router.HandleFunc("GET /", s.handler.Home)
+	router.HandleFunc("GET /catalog", s.handler.Catalog)
+	router.HandleFunc("GET /category/{id}", s.handler.ProductsByCategory)
+	router.HandleFunc("GET /product/{id}", s.handler.Product)
+
+	return router
+}
