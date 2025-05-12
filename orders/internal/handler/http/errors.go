d9031cff1e1129bb958126fce3abdda97c41214c
Maksim Kot <kotmaksimarturovich@gmail.com>
2025-05-13 01:07:47 +0300

    feat(orders): implement handlers for HTTP interactions

---
+++ orders/internal/handler/http/errors.go
@@ -0,0 +1,35 @@
+package http
+
+import (
+	"log"
+	"net/http"
+)
+
+func (h *Handler) logError(r *http.Request, err error) {
+	log.Printf("url: %s %v", r.URL, err)
+}
+
+func (h *Handler) errorResponse(w http.ResponseWriter, r *http.Request, status int, message any) {
+	env := envelope{"error": message}
+
+	err := h.writeJSON(w, status, env, nil)
+	if err != nil {
+		h.logError(r, err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+}
+
+func (h *Handler) serverErrorResponse(w http.ResponseWriter, r *http.Request, err error) {
+	h.logError(r, err)
+	message := "the server encountered a problem and could not process your request"
+	h.errorResponse(w, r, http.StatusInternalServerError, message)
+}
+
+func (h *Handler) notFoundResponse(w http.ResponseWriter, r *http.Request) {
+	message := "the requested resource could not be found"
+	h.errorResponse(w, r, http.StatusNotFound, message)
+}
+
+func (h *Handler) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
+	h.errorResponse(w, r, http.StatusBadRequest, err.Error())
+}
---
+++ orders/internal/handler/http/http.go
@@ -0,0 +1,128 @@
+package http
+
+import (
+	"context"
+	"errors"
+	"net/http"
+	"time"
+
+	"github.com/Maksim-Kot/Tech-store-orders/config"
+	"github.com/Maksim-Kot/Tech-store-orders/internal/controller/orders"
+	"github.com/Maksim-Kot/Tech-store-orders/pkg/model"
+)
+
+type Handler struct {
+	ctrl *orders.Controller
+	cfg  config.APIConfig
+}
+
+func New(ctrl *orders.Controller, cfg config.APIConfig) *Handler {
+	return &Handler{ctrl, cfg}
+}
+
+func (h *Handler) HealthcheckHandler(w http.ResponseWriter, r *http.Request) {
+	env := envelope{
+		"status": "available",
+		"system_info": map[string]string{
+			"environment": h.cfg.Env,
+			"version":     h.cfg.Version,
+			"name":        h.cfg.Name,
+		},
+	}
+
+	err := h.writeJSON(w, http.StatusOK, env, nil)
+	if err != nil {
+		h.serverErrorResponse(w, r, err)
+	}
+}
+
+func (h *Handler) CreateOrderHandler(w http.ResponseWriter, r *http.Request) {
+	var input struct {
+		UserID int64        `json:"user_id"`
+		Price  float64      `json:"price"`
+		Items  []model.Item `json:"items"`
+	}
+
+	err := h.readJSON(w, r, &input)
+	if err != nil {
+		h.badRequestResponse(w, r, err)
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	id, err := h.ctrl.CreateOrder(ctx, input.UserID, input.Price, input.Items)
+	if err != nil {
+		switch {
+		case errors.Is(err, orders.ErrNotCreated):
+			h.badRequestResponse(w, r, err)
+		default:
+			h.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	message := map[string]int64{
+		"id": id,
+	}
+
+	err = h.writeJSON(w, http.StatusCreated, envelope{"order": message}, nil)
+	if err != nil {
+		h.serverErrorResponse(w, r, err)
+	}
+}
+
+func (h *Handler) OrderByIDHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := h.getID(r)
+	if err != nil || id < 1 {
+		h.notFoundResponse(w, r)
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	order, err := h.ctrl.OrderByID(ctx, id)
+	if err != nil {
+		switch {
+		case errors.Is(err, orders.ErrNotFound):
+			h.notFoundResponse(w, r)
+		default:
+			h.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	err = h.writeJSON(w, http.StatusOK, envelope{"order": order}, nil)
+	if err != nil {
+		h.serverErrorResponse(w, r, err)
+	}
+}
+
+func (h *Handler) OrdersByUserIDHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := h.getID(r)
+	if err != nil || id < 1 {
+		h.notFoundResponse(w, r)
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	purchases, err := h.ctrl.OrdersByUserID(ctx, id)
+	if err != nil {
+		switch {
+		case errors.Is(err, orders.ErrNotFound):
+			h.notFoundResponse(w, r)
+		default:
+			h.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	err = h.writeJSON(w, http.StatusOK, envelope{"orders": purchases}, nil)
+	if err != nil {
+		h.serverErrorResponse(w, r, err)
+	}
+}
